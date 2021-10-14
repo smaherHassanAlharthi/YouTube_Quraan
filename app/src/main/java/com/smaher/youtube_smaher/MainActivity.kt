62adc33e295fc042090e7aba941ef6dbfa507bec
smaher <smaher.1416.it@gmail.com>
2021-10-14 04:55:41 +0300

    first commit

---
+++ app/src/main/java/com/smaher/youtube_smaher/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.smaher.youtube_smaher
+
+import android.content.Context
+import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+
+class MainActivity : AppCompatActivity() {
+    private val videos: Array<Array<String>> = arrayOf(
+        arrayOf("سورة القارعة", "bX9f3unK1Bg"),
+        arrayOf("سورة الناس ", "nvRz-ZBWOZw"),//
+        arrayOf("سورة العصر", "p8mZ-9MVqyM"),
+        arrayOf("سورة التكاثر", "jDgREf7_JHQ"))
+    private lateinit var youTubePlayerView: YouTubePlayerView
+    private lateinit var player: YouTubePlayer
+    private var currentVideo = 0
+    private var timeStamp = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkInternet()
+
+        youTubePlayerView = findViewById(R.id.ytPlayer)
+        youTubePlayerView.addYouTubePlayerListener(object: AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                super.onReady(youTubePlayer)
+                player = youTubePlayer
+                player.loadVideo(videos[currentVideo][1], timeStamp)
+                initializeRV()
+            }
+        })
+
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE) {
+            youTubePlayerView.enterFullScreen()
+        } else if (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
+            youTubePlayerView.exitFullScreen()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("currentVideo", currentVideo)
+        outState.putFloat("timeStamp", timeStamp)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        currentVideo = savedInstanceState.getInt("currentVideo", 0)
+        timeStamp = savedInstanceState.getFloat("timeStamp", 0f)
+    }
+
+    private fun initializeRV(){
+        val recyclerView: RecyclerView = findViewById(R.id.rvVideos)
+        recyclerView.adapter = VideoAdapter(videos, player)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+    }
+
+    private fun checkInternet(){
+        if(!connectedToInternet()){
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("Internet Connection Not Found")
+                .setPositiveButton("RETRY"){_, _ -> checkInternet()}
+                .show()
+        }
+    }
+
+    private fun connectedToInternet(): Boolean{
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
+}
